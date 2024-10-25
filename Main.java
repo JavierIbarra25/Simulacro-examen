a1069dcb76900dca326299626424801354e68ae6
Javier_Ibarra <javiermaria.s463101@cesurformacion.com>
2024-10-25 09:00:20 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+public static Main{
+
+    public static void main(String[] args){
+        max(2,2);
+        }
+
+    public static int max (int x, int y){
+    
+    if(x>=y){
+        return x;}
+    else{return y;}
+
+    }
+}
